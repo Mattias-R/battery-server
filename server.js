8a2a591b19ff8b413da9c7737769a3bf9f21282b
Mattias <42864156+Mattias-R@users.noreply.github.com>
2021-06-26 12:37:52 +0200

    done

--- server.js
+++ server.js
@@ -99,8 +99,9 @@ app.post('/register', checkNotAuthenticated, async (req, res) => {
             id: Date.now().toString(),
             name: req.body.name,
             email: req.body.email,
-            password: hashedPassword
+            password: hashedPassword,
         })
+        console.log(users)
         res.redirect('/login')
     } catch {
         res.redirect('/register')
