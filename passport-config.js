83f225d63966d2639c225544743efc404612aa42
AnnaLeitner <71489265+AnnaLeitner@users.noreply.github.com>
2021-06-27 16:04:21 +0200

    comments

--- passport-config.js
+++ passport-config.js
@@ -1,30 +1,30 @@
-const LocalStrategy = require('passport-local').Strategy
+const LocalStrategy = require('passport-local').Strategy// local st
 const bcrypt = require('bcrypt')
 
 
-function initialize(passport, getUserByEmail, getUserById){
-    const authenticateUser  = async (email, password, done) =>{
-        const user = getUserByEmail(email)
+function initialize(passport, getUserByEmail, getUserById){//passport related func
+    const authenticateUser  = async (email, password, done) =>{//error func and async and done func..if done its done
+        const user = getUserByEmail(email)//get user by email
         if(user == null){
-            return done(null, false, {message: 'no user with that email'})
+            return done(null, false, {message: ' No user with that email'})//cannot find user but not wrong with server so no error
         }
-        try{
-            if(await bcrypt.compare(password, user.password)){
-                return done(null, user)
+        try{//to compare pw we use bcrypt if pw is pw
+            if(await bcrypt.compare(password, user.password)){// first pw is which the user send in with the login form compare to user.pw
+                return done(null, user)// if ture authenticated user
             }else{
-                return done(null, false, {message: 'Password incorrect'})
+                return done(null, false, {message: 'Password incorrect'})//if not
             }
         }catch(error){
             console.log(error)
-            return done(error)
+            return done(error)// error with application
         }
     }
 
-    passport.use(new LocalStrategy({usernameField: 'email'},
-        authenticateUser))
-    passport.serializeUser((user,done) => { done(null, user.id)})
-    passport.deserializeUser((id,done) => {
+    passport.use(new LocalStrategy({usernameField: 'email'},// default is user we have email
+        authenticateUser))// func line 6
+    passport.serializeUser((user,done) => { done(null, user.id)})// serialize our user in our session
+    passport.deserializeUser((id,done) => {// deserializer
         return done(null, getUserById(id))
     })
 }
-module.exports = initialize
+module.exports = initialize//to use func
--- server.js
+++ server.js
@@ -1,27 +1,32 @@
+//load all environment vars and set in process.env
 if(process.env.NODE_ENV !== 'production'){
     require('dotenv').config()
 }
 
 
 //holt sich die express lib
-const express = require('express');
+const express = require('express');// import express
 const bodyParser = require('body-parser');
-const passport = require('passport')
+const passport = require('passport')// for line 24
+
 //damit wird die express funktion ausgefuehrt.
 const app = express();
+
 //setzt port als port 1337 fest
 const port = process.env.PORT || 1337;
-const bcrypt = require('bcrypt')
+
+const bcrypt = require('bcrypt')// allow to hash pw and to compare hashed pw for security
+
 const methodOverride = require('method-override')
 
-const flash = require('express-flash')
+const flash = require('express-flash')//massage if something failed is used by passport internal eg wrong email
 const session = require('express-session')
 
-const initializePassport = require('./passport-config')
+const initializePassport = require('./passport-config')//
 
-initializePassport(passport, email =>
-        users.find(user => user.email === email),
-    id => users.find(user => user.id === id)
+initializePassport(passport, email =>//takes email
+        users.find(user => user.email === email),//find user with email
+    id => users.find(user => user.id === id)// compare id
 )
 
 //wird gebraucht fuer die verbindung von der webseite zum server
@@ -29,24 +34,24 @@ let cors = require('cors')
 app.use(cors()) // Use this after the variable declaration
 
 
-const users = []
+const users = []// to store instead database...weiß nicht ob ich das so lassen sollte?
 
-app.set('view-engine', 'ejs')
-app.use('/css',express.static(__dirname + '/css'));
+app.set('view-engine', 'ejs')//view engine is set to ejs
+app.use('/css',express.static(__dirname + '/css'));//files we use
 app.use('/images',express.static(__dirname + '/images'));
 app.use('/js',express.static(__dirname + '/js'));
 app.use(methodOverride('_method'))
 
 
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({extended: false}))// tells application to take forms from login and register inside req var inside post
 app.use(flash())
 app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
+    secret: process.env.SESSION_SECRET,// a key to keep all info encrypted caps is name
+    resave: false,//if nothing has changed i dont wanna resave
     saveUninitialized: false
 }))
 app.use(passport.initialize())
-app.use(passport.session())
+app.use(passport.session())//store vars the entire session work with line 48
 
 /*
 app.get('/',checkAuthenticated, (req, res) =>{
@@ -55,11 +60,11 @@ app.get('/',checkAuthenticated, (req, res) =>{
 
  */
 
-app.get('/checkout',checkAuthenticated, (req, res) =>
+app.get('/checkout',checkAuthenticated, (req, res) =>//
     res.render('checkout.ejs', {name: req.user.name})
 )
-app.delete('/logout', (req, res) => {
-    req.logOut()
+app.delete('/logout', (req, res) => {//delete req saver
+    req.logOut()// from passport to log out and clear session
     res.redirect('/login')
 })
 
@@ -76,37 +81,39 @@ app.get('/', (req, res) =>{
 
 */
 
-app.get('/login',checkNotAuthenticated, (req,res) =>{
-    res.render('login.ejs')
+app.get('/login',checkNotAuthenticated, (req,res) =>{//render to login //not allow users to go to login if logged in
+    res.render('login.ejs')// no extra info needed
 })
 
-app.post('/login',checkNotAuthenticated, passport.authenticate('local', {
-    successRedirect: 'checkout',
-    failureRedirect: '/login',
-    failureFlash: true
+app.post('/login',checkNotAuthenticated, passport.authenticate('local', {// local for local strategy
+    successRedirect: 'checkout',// where when success
+    failureRedirect: '/login',// redirect if failure
+    failureFlash: true//show message to user
 }))
 
-app.post('/login', checkNotAuthenticated, (req,res) =>{
+app.post('/login', checkNotAuthenticated, (req,res) =>{//dont allow to login if authenticated
     res.render('login.ejs')
 })
 
-app.get('/register', checkNotAuthenticated, (req, res) => {
+app.get('/register', checkNotAuthenticated, (req, res) => {// same as login with register
     res.render('register.ejs')
 })
 
-app.post('/register', checkNotAuthenticated, async (req, res) => {
-    try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+app.post('/register', checkNotAuthenticated, async (req, res) => {// async func
+    try {// for async code
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)// 10 default standard quick but secure await bc async
+        // hashedpw to store
         users.push({
-            id: Date.now().toString(),
-            name: req.body.name,
-            email: req.body.email,
-            password: hashedPassword,
+            id: Date.now().toString(),// unique identifier
+            name: req.body.name,// get name
+            email: req.body.email,// get email
+            password: hashedPassword,// use hashedpw
         })
-        console.log(users)
-        res.redirect('/login')
+        console.log(req.method)// see if real post
+        console.log(users)// see if user added
+        res.redirect('/login')// redirect to login
     } catch {
-        res.redirect('/register')
+        res.redirect('/register')// redirect if failed
     }
 })
 
@@ -117,17 +124,17 @@ app.use(bodyParser.json());
 
 //continue to the next matching route handler
 function checkAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        return next()
+    if (req.isAuthenticated()) {//isAuthenticated returns true if there is a user
+        return next()// do the next thing?
     }
-    res.redirect('/login')
+    res.redirect('/login')//false if no user authenticated
 }
 
-function checkNotAuthenticated(req, res, next) {
+function checkNotAuthenticated(req, res, next) {//similar to func 126
     if (req.isAuthenticated()) {
-        return res.redirect('/checkout')
+        return res.redirect('/checkout')// if true redirect to checkout
     }
-    next()
+    next()/// continue
 }
 
 
